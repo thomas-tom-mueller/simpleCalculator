baaf471419aaa099cc458bd07852fa2727d90518
Thomas Müller <thomas_mueller_@web.de>
2021-03-20 09:01:20 +0100

    clean up pom + gitignore

--- simpleCalculator/src/test/java/simpleCalculator/SimpleCalculatorTest.java
+++ simpleCalculator/src/test/java/simpleCalculator/SimpleCalculatorTest.java
@@ -2,17 +2,14 @@ package simpleCalculator;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
-//@RunWith(MockitoJUnitRunner.class)
-
 public class SimpleCalculatorTest {
 
     SimpleCalculator simpleCalculator = new SimpleCalculator();
